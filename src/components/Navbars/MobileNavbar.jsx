343956dfccb08a39ab6c3ed8fad8433e59f120d4
Aditya Prajapati <63953219+Aditya-Prajapati@users.noreply.github.com>
2023-06-22 16:22:49 +0530

    Update MobileNavbar.jsx

--- src/components/Navbars/MobileNavbar.jsx
+++ src/components/Navbars/MobileNavbar.jsx
@@ -3,32 +3,44 @@ import NavItem from "./NavItem";
 import "./MobileNavbar.css";
 import ProfileImage from "../ProfileImage";
 import { faHouse, faHashtag, faUser } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-export default function MobileNavbar(){
+export default function MobileNavbar(props){
+
+    const navigate = useNavigate();
 
     function handleLogout(){
-        window.open("https://twitter-clone-frontend-in-progress.vercel.app/auth/logout", "_self");
+        axios.post("https://twitter-clone-backend-in-progress.vercel.app/auth/logout",
+        {},
+        { withCredentials: true }
+        )
+        .then((res) => {
+            navigate("/");
+        })
+        .catch((err) => {
+            console.log(err);
+        })
     }
 
     return (
-        <div className="fixed-bottom bgc-white mobile-navbar">
+        <div className="fixed-bottom mobile-navbar">
 
-            <ul className="nav justify-content-between mx-5">
-                <NavItem link={"/home"} iconName={faHouse} iconColor={"black"} iconSize={"l"} style={{ padding: ".5rem" }} /> 
-                <NavItem link={"/explore"} iconName={faHashtag} iconColor={"black"} iconSize={"l"} style={{ padding: ".5rem" }} />
-                <NavItem link={"/profile"} iconName={faUser} iconColor={"black"} iconSize={"l"} style={{ padding: ".5rem" }} /> 
-                {/* Add logout functioality for mobiles */}
-                {/* <div className="dropdown">
-                    <a href="#" className="d-flex align-items-center justify-content-center p-3 link-body-emphasis dropdown-toggle" data-bs-toggle="dropdown">
-                        <ProfileImage width={14} height={14} />
-                    </a>
-
-                    <ul className="dropdown-menu text-small shadow">
-                        <li>
-                            <a className="dropdown-item" href="#" onClick={handleLogout}> Logout </a>
-                        </li>
-                    </ul>
-                </div> */}
+            <ul className="nav justify-content-between align-items-center mx-4">
+                <NavItem link={"/home"} iconName={faHouse} iconColor={"black"} iconSize={"l"} /> 
+                <NavItem link={"/explore"} iconName={faHashtag} iconColor={"black"} iconSize={"l"} />
+                <NavItem link={"/profile"} iconName={faUser} iconColor={"black"} iconSize={"l"} /> 
+                
+                <div className="dropdown">
+                    <div href="#" className="d-flex align-items-center justify-content-center link-body-emphasis dropdown-toggle" data-bs-toggle="dropdown">
+                        <ProfileImage width={28} height={28} user={props.user} />
+                        <ul className="dropdown-menu text-small shadow">
+                            <li>
+                                <a className="dropdown-item" href="#" onClick={handleLogout}> Logout </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </ul>
 
         </div>
